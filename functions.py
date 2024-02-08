399fbe2707cbc29459f096151b0b683a201fde3b
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-08 13:58:05 -0300

    Aula 145

--- functions.py
+++ functions.py
@@ -3,14 +3,15 @@ def multiply(x, y):
     return result
 
 
-answer = multiply(10.5, 4)
-print(answer)
+def is_palindrome(string):
+    # backwards = string[::-1]
+    # return backwards == string
+    return string[::-1] == string
 
-forty_two = multiply(6, 7)
-print(forty_two)
 
-print()
-
-for val in range(1, 5):
-    two_times = multiply(2, val)
-    print(two_times)
+word = input("Please enter a word to check: ")
+print(word[::-1])
+if is_palindrome(word):
+    print("'{}' is a palindrome".format(word))
+else:
+    print("'{}' is not a palindrome".format(word))
