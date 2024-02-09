b814cae45319d39a2476b9686853c0a4e9f2fdae
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-09 13:40:32 -0300

    Aula 173

--- colour_print.py
+++ colour_print.py
@@ -1,3 +1,5 @@
+import colorama
+
 # Some ANSI escape sequences for colours and effects
 BLACK = '\u001b[30m'
 RED = '\u001b[31m'
@@ -26,6 +28,7 @@ def colour_print(text: str, effect: str) -> None:
     print(output_string)
 
 
+colorama.init()
 colour_print("Hello, Red", RED)
 # test that the colour was reset
 print("This should be in the default terminal colour")
@@ -35,3 +38,4 @@ colour_print("Hello, Bold", BOLD)
 colour_print("Hello, Underline", UNDERLINE)
 colour_print("Hello, Reverse", REVERSE)
 colour_print("Hello, Black", BLACK)
+colorama.deinit()
