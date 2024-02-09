9c5b4156c73c77e766aa19f72bfadd40e4b6546a
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-09 09:21:54 -0300

    Aula 147

--- functions.py
+++ functions.py
@@ -6,12 +6,21 @@ def multiply(x, y):
 def is_palindrome(string):
     # backwards = string[::-1]
     # return backwards == string
-    return string[::-1] == string
+    return string[::-1].casefold() == string.casefold()
+
+
+def palindrome_sentence(sentence):
+    string = ""
+    for char in sentence:
+        if char.isalnum():
+            string += char
+    print(string)
+    # return string[::-1].casefold() == string.casefold()
+    return is_palindrome(string)
 
 
 word = input("Please enter a word to check: ")
-print(word[::-1])
-if is_palindrome(word):
+if palindrome_sentence(word):
     print("'{}' is a palindrome".format(word))
 else:
     print("'{}' is not a palindrome".format(word))
