447b10878156a83d1840cb8ba353db963b707c38
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-09 12:18:02 -0300

    Aula 160

--- guessingame.py
+++ guessingame.py
@@ -2,6 +2,16 @@ import random
 
 
 def get_integer(prompt):
+    """
+    Get an integer from Standard Input (stdin).
+
+    The function will continue looping, and prompting
+    the user, until a valid `int` is entered.
+
+    :param prompt: The String that the user will see, when
+        they're prompted to enter the value.
+    :return: The integer that the user enters.
+    """
     while True:
         temp = input(prompt)
         if temp.isnumeric():
@@ -11,10 +21,12 @@ def get_integer(prompt):
         print("{} is not a valid number.".format(temp))
 
 
+print(input.__doc__)
+print("*" * 80)
 highest = 1000
 answer = random.randint(1, highest)
 print(answer)   # TODO: Remove after testing
-guess = 0 # initialise to any number that doesn't equal the answer
+guess = 0  # initialise to any number that doesn't equal the answer
 print("Please guess number between 1 and {}: ".format(highest))
 
 while guess != answer:
